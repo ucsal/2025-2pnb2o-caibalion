467097fe2897ab2e3ab6497b7d301177e2d93ffb
Lucas Alex <lucasalexti@gmail.com>
2025-09-04 12:11:54 -0300

    classes concretas para as politicas

---
+++ src/main/java/br/com/mariojp/solid/ocp/PartnerPolicy.java
@@ -0,0 +1,8 @@
+package br.com.mariojp.solid.ocp;
+
+public class PartnerPolicy implements DiscountPolicy {
+    @Override
+    public double apply(double amount) {
+        return amount * 0.88;
+    }
+}
---
+++ src/main/java/br/com/mariojp/solid/ocp/PremiumPolicy.java
@@ -0,0 +1,8 @@
+package br.com.mariojp.solid.ocp;
+
+public class PremiumPolicy implements DiscountPolicy {
+    @Override
+    public double apply(double amount) {
+        return amount * 0.90;
+    }
+}
